35fe627c9bbf047d8e334981759b1db89712c0e9
Kj-RahiL <rahiilarham@gmail.com>
2023-12-22 19:40:02 +0600

    update and deploy

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uma9m7n.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -35,6 +35,12 @@ async function run() {
         app.post('/todoList', async (req, res) => {
             const todo = req.body;
             const result = await todoCollection.insertOne(todo)
+            res.send(result)
+        })
+        app.delete('/todoList/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await todoCollection.deleteOne(query)
             res.send(result)
         })
 
